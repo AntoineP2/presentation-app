e3b5970cc131f982a65e7e008d87567e993341de
a.vinsonneau <antoine.vinsonneau@compufirst.com>
2022-11-16 12:37:30 +0100

    iun

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <display :count="this.number" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import display from "./components/display.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    display,
+  },
+  data() {
+    return {
+      number: 0,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
+body {
+  background-color: #31572c;
+  color: white;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +30,34 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#buttonCount {
+  position: relative;
+  top: 100px;
+}
+.buttonCount {
+  transition-duration: 0.3s;
+  background-color: #4f772d;
+  padding: 0 20px 0 20px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+}
+
+.buttonCount:hover {
+  transition-duration: 0.3s;
+  background-color: #132a13;
+  transform: scale(0.95);
+}
+
+.buttonCount p {
+  font-family: "Alfa Slab One", cursive;
+  font-size: 30px;
+  color: #ecf39e;
+}
+
+.conteneur {
+  display: flex;
+  justify-content: center;
+  width: 100%;
 }
 </style>
